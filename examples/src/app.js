47ad6cc69fc4863407c330d4e37657ddf1fccc37
Matthew King <matt@keepertax.com>
2023-07-27 22:13:09 -0700

    init

---
+++ examples/src/app.js
@@ -0,0 +1,31 @@
+/* eslint react/prop-types: 0 */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './example.less';
+
+import Creatable from './components/Creatable';
+import CustomComponents from './components/CustomComponents';
+import CustomRender from './components/CustomRender';
+import Multiselect from './components/Multiselect';
+import NumericSelect from './components/NumericSelect';
+import BooleanSelect from './components/BooleanSelect';
+import States from './components/States';
+import Search from './components/Search';
+
+ReactDOM.render(
+	<div>
+		<Search label="Search" />
+		<States label="States" searchable />
+		<Multiselect label="Multiselect" />
+		<NumericSelect label="Numeric Values" />
+		<BooleanSelect label="Boolean Values" />
+		<CustomRender label="Custom Render Methods"/>
+		<CustomComponents label="Custom Placeholder, Option, Value, and Arrow Components" />
+		<Creatable
+			hint="Enter a value that's NOT in the list, then hit return"
+			label="Custom tag creation"
+		/>
+	</div>,
+	document.getElementById('example')
+);
---
+++ examples/src/components/Search.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import createClass from 'create-react-class';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+
+const STATES = require('../data/searchStates');
+
+var SearchField = createClass({
+	displayName: 'SearchField',
+	propTypes: {
+		label: PropTypes.string,
+	},
+	getInitialState () {
+		return {
+			selectValue: null,
+		};
+	},
+	updateValue (newValue) {
+		this.setState({
+			selectValue: newValue,
+		});
+	},
+	render () {
+		var options = STATES.US;
+		return (
+			<div className="section">
+				<h3 className="section-heading">{this.props.label}</h3>
+				<Select
+					id="state-select"
+					ref={(ref) => { this.select = ref; }}
+					onBlurResetsInput={false}
+					onSelectResetsInput={false}
+					autoFocus
+					options={options}
+					simpleValue
+					name="selected-state"
+					value={this.state.selectValue}
+					onChange={this.updateValue}
+					searchable
+				/>
+			</div>
+		);
+	}
+});
+
+
+module.exports = SearchField;
